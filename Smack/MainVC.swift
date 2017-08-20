f53d7adf9795f2e34a04dcbe9a271c6efa7f5949
Ken Krippeler <kriken19@gmail.com>
2017-08-20 14:44:43 +0200

    Few Setups

---
+++ Smack/MainVC.swift
@@ -0,0 +1,23 @@
+//
+//  MainVC.swift
+//  Smack
+//
+//  Created by Ken Krippeler on 20.08.17.
+//  Copyright © 2017 Lichtverbunden. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+ 
+
+}
+
