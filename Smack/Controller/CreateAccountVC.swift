cb3571292704689adec6079b4c2c7356cb36913d
Ken Krippeler <kriken19@gmail.com>
2017-08-22 21:55:26 +0200

    Added register user function and buttons as well as corner radius to buttons

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class CreateAccountVC: UIViewController
 {
-
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -18,6 +22,29 @@ class CreateAccountVC: UIViewController
         
     }
 
+    @IBAction func createAccountPressed(_ sender: UIButton)
+    {
+        guard let email = emailText.text , emailText.text != "" else{ return }
+        guard let password = passwordText.text , passwordText.text != "" else{ return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+                if success
+                {
+                    print("registered user!")
+                }
+            }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: UIButton)
+    {
+    
+    }
+    
+    @IBAction func pickbackgroundColorPressed(_ sender: UIButton)
+    {
+    
+    }
+    
     @IBAction func closePressed(_ sender: UIButton)
     {
         performSegue(withIdentifier: UNWIND, sender: nil)
