85f2f68b8137f7e60b9d49ded4393454a9ce170b
Ken Krippeler <kriken19@gmail.com>
2017-08-28 17:44:46 +0200

    Added Login function to LoginVC

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -23,6 +23,11 @@ class ChannelVC: UIViewController
         
     }
     
+    override func viewDidAppear(_ animated: Bool)
+    {
+        setupUserInfo()
+    }
+    
     @IBAction func loginButtonPressed(_ sender: UIButton)
     {
         if AuthService.instance.isLoggedIn
@@ -38,6 +43,11 @@ class ChannelVC: UIViewController
     }
     
     @objc func userDataDidChange(_ notif: Notification)
+    {
+        setupUserInfo()
+    }
+    
+    func setupUserInfo()
     {
         if AuthService.instance.isLoggedIn
         {
--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class LoginVC: UIViewController
 {
-
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        setupView()
     }
 
     @IBAction func closePressed(_ sender: UIButton)
@@ -27,4 +30,49 @@ class LoginVC: UIViewController
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
     
+    @IBAction func loginPressed(_ sender: UIButton)
+    {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = usernameText.text, usernameText.text != "" else {return}
+        guard let password = passwordText.text, passwordText.text != "" else {return}
+        
+        AuthService.instance.loginUser(email: email, password: password)
+        { (success) in
+            if success
+            {
+                AuthService.instance.findUserByEmail(completion:
+                    {(success) in
+                        if success
+                        {
+                            NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                            self.spinner.isHidden = true
+                            self.spinner.stopAnimating()
+                            
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    })
+            }
+        }
+    }
+    
+    func setupView()
+    {
+        spinner.isHidden = true
+        usernameText.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        passwordText.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(LoginVC.handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap()
+    {
+        view.endEditing(true)
+    }
+    
+    
+    
+
 }
