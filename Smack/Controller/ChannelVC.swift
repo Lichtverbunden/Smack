e78ba7579c946f4b7f678d8fefccca00916ce158
Ken Krippeler <kriken19@gmail.com>
2017-09-02 18:15:41 +0200

    Added channel tableview functions

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ChannelVC: UIViewController
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var userImage: CircleImage!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
@@ -17,6 +18,8 @@ class ChannelVC: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
@@ -60,6 +63,31 @@ class ChannelVC: UIViewController
             loginButton.setTitle("Login", for: .normal)
             userImage.image = UIImage(named: "menuProfileIcon")
             userImage.backgroundColor = UIColor.clear
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return MessageService.instance.channels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell
+        {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            
+            return cell
+        }
+        else
+        {
+            return ChannelCell()
         }
     }
 }
